47aa2dcf91658fadfd901553f976ac93929bbde7
loic metrailler <loic@metrailler.me>
2025-12-02 13:20:41 +0100

    [FIX] backend

--- bermude-sarl-storefront/src/app/[countryCode]/(main)/page.tsx
+++ bermude-sarl-storefront/src/app/[countryCode]/(main)/page.tsx
@@ -6,9 +6,8 @@ import { listCollections } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
-  description:
-    "A performant frontend ecommerce starter template with Next.js 15 and Medusa.",
+  title: "Bermude",
+  description: "Votre magasin d'habits situé à Lausanne.",
 }
 
 export default async function Home(props: {
--- bermude-sarl-storefront/src/modules/layout/components/side-menu/index.tsx
+++ bermude-sarl-storefront/src/modules/layout/components/side-menu/index.tsx
@@ -98,7 +98,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                         />
                       </div>
                       <Text className="flex justify-between txt-compact-small">
-                        © {new Date().getFullYear()} Medusa Store. All rights
+                        © {new Date().getFullYear()} Bermude. All rights
                         reserved.
                       </Text>
                     </div>
